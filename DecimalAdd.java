67d536a3ba061f5bb51c7991e4a0ec458a2b7903
chasingtowardssun <43771132+chasingtowardssun@users.noreply.github.com>
2019-01-03 22:50:54 -0800

    Create DecimalAdd.java

---
+++ DecimalAdd.java
@@ -0,0 +1,33 @@
+public static String add(String a,String b) {
+                int index1=a.indexOf('.');
+                int index2=b.indexOf('.');
+                int aleft=a.length()-1-index1;
+                int bleft=b.length()-1-index2;
+                if(bleft>aleft) {
+                        return add(b,a);
+                }
+                
+                int astart=a.length()-1;
+                int bstart=a.length()-1+index2-index1;
+
+                StringBuilder sb=new StringBuilder();
+                int carry=0;
+                while(astart>=0 ||(bstart>=0 && bstart<b.length())||carry!=0) {
+                        if(astart==index1 && bstart==index2) {
+                           sb.append('.');        
+                        } else {
+                                
+                                if(astart>=0) {
+                                        carry +=a.charAt(astart)-'0';-baidu 1point3acres
+                                }
+                                if (bstart>=0 && bstart<b.length()) {
+                                        carry +=b.charAt(bstart)-'0';
+                                }
+                                sb.append(carry%10);
+                                carry/=10;
+                        }
+            astart--;
+            bstart--;
+                }
+                return sb.reverse().toString();
+        }
